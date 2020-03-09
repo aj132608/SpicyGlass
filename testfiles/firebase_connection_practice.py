4e78f62c3e17746abc20d79425a23c0c44d5c44a
Michael <michael.bishop@ttu.edu>
2020-03-09 11:58:52 -0500

    code change

--- testfiles/firebase_connection_practice.py
+++ testfiles/firebase_connection_practice.py
@@ -5,6 +5,21 @@ if __name__ == "__main__":
     with open('creds.json') as file:
         creds = json.load(file)
 
-    firebase_obj = firebase.FirebaseApplication(creds['databaseURL'])
-    name = firebase_obj.get(creds['databaseURL'], 'user')
-    print(name)
+    firebase_obj = firebase.FirebaseApplication(creds['databaseURL'], None)
+    name = firebase_obj.get('user', None)
+    # print(name)
+    # response = firebase_obj.post('user', 'YA BOI')
+    # returns Key for the value that was posted
+    # response_key = response['name']
+    # print("Key: ", response_key)
+
+    # response = firebase_obj.put('', 'car-on', False)
+    # print(response)
+
+    print((firebase_obj.get('', None)))
+    data = {
+        'front': False,
+        'back': False
+    }
+    firebase_obj.put('car-on', '', True)
+    print((firebase_obj.get('car-on', None)))
